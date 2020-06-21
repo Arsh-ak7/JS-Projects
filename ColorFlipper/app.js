ba5d99a5660f86b9ae66e513d4373494d5a33974
Arsh-ak7 <arshkr07@gmail.com>
2020-06-21 11:10:01 +0530

    Added Files

--- ColorFlipper/app.js
+++ ColorFlipper/app.js
@@ -1,1 +1,14 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,12,100)", "#f15025"];
+const btn= document.getElementById('btn');
+const color = document.querySelector(".color");//gives span showing which coloer is used
+
+btn.addEventListener("click", function(){
+    //Multiply by arr size to get values less than it
+    const randomNumber=Math.floor(getRandomNumbers()*4); //USed to get arrayIndexshd be betwwwn 0-3 because 3 elements in arr
+    document.body.style.backgroundColor=colors[randomNumber];
+    color.textContent=colors[randomNumber];
+});
+
+function getRandomNumbers(){
+    return Math.random(); // returns numbers between 0-1
+}
