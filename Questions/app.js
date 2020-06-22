56698ac86ca02f5fb3dee328a99f1fc82155316d
Arsh-ak7 <arshkr07@gmail.com>
2020-06-22 12:12:41 +0530

    Done Questions

--- Questions/app.js
+++ Questions/app.js
@@ -1,2 +1,25 @@
-//using selectors inside the element
-// traversing the dom
+//1st method using selectors inside the element
+const question= document.querySelectorAll('.question');
+
+question.forEach(function(e){
+    const btn= e.querySelector('.question-btn');//Do console.log to see what happens
+    btn.addEventListener("click",function(){
+        question.forEach(function(item){
+            if(item!==e)//Item returns the list of articles
+            item.classList.remove('show-text');
+        })
+        e.classList.toggle('show-text');
+    })
+});
+
+
+// //2nd Method traversing the dom
+
+// const questionBtn = document.querySelectorAll('.question-btn');
+// questionBtn.forEach(function(btn){
+//     btn.addEventListener("click", function(e){
+//         const question= e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
+
